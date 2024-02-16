e2decd83357418c20c235b896d5e4bb0d45a8ae1
doremansunio <136595579+doremansunio@users.noreply.github.com>
2024-02-16 22:24:10 +0530

    a3

--- main.tf
+++ main.tf
@@ -11,12 +11,42 @@ provider "rafay" {
   # provider_config_file = "./rafay_config.json"
 }
 
-resource "rafay_project" "rafay_proj_new" {  
+resource "rafay_environment_template" "aws-et-example" {
+  depends_on = [ rafay_project.rafay_proj_new ]
   metadata {
-    name        = "t1"
-    description = "terraform project"
+    name    = "waas-env-template"
+    project = "centralpool"
   }
   spec {
-    default = false    
+    version = "v1"
+    resources {
+      type = "dynamic"
+      kind = "resourcetemplate"
+      name = "res-setup-rafay-project"
+      resource_options {
+        version   = "v1"
+        //dedicated = true
+      }      
+    } 
+    resources {
+      type = "dynamic"
+      kind = "resourcetemplate"
+      name = "res-setup-rafay-prj-network-policies"
+      resource_options {
+        version   = "v1"
+        //dedicated = true
+      }
+      depends_on {
+        name = "res-setup-rafay-project"
+      }
+    }        
+    sharing {
+      projects {
+        name = "team1"
+      }
+      projects {
+        name = "team2"
+      }
+    }    
   }
 }
