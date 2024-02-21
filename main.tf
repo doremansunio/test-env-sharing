5cd34ffe98fe5729d5ed26168827c30577839a03
doremansunio <136595579+doremansunio@users.noreply.github.com>
2024-02-21 11:37:33 +0530

    g1

--- main.tf
+++ main.tf
@@ -11,13 +11,34 @@ provider "rafay" {
   # provider_config_file = "./rafay_config.json"
 }
 
-resource "rafay_cluster_sharing" "demo-terraform-specific" {  
-  clustername = "eks-cluster"
-  project     = "central-pool"
-  sharing {
-    all = false
-    projects {
-      name = "defaultproject"
-    }    
+# resource "rafay_cluster_sharing" "demo-terraform-specific" {  
+#   clustername = "eks-cluster"
+#   project     = "central-pool"
+#   sharing {
+#     all = false
+#     projects {
+#       name = "defaultproject"
+#     }    
+#   }
+# }
+
+
+resource "rafay_environment_template" "aws-et-example" {  
+  metadata {
+    name    = "naas-envir-template"
+    project = "centralpool"
+  }
+  spec {
+    version = "v1"
+
+    resources {
+    }
+    sharing {
+      enabled = true
+
+      projects {
+        name = "defaultproject"
+      }
+    }
   }
 }
