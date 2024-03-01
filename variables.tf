0e095fb2203324b5077982387952f21c802301c3
doremansunio <136595579+doremansunio@users.noreply.github.com>
2024-03-01 15:17:54 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "central_pool_name" {
+  type = string    
+}
+
+variable "project_name" {
+  type = string  
+} 
+
+variable "cluster_name" {
+  type = string
+}
+
+variable "workspace_admins" {
+  type    = list  
+}
