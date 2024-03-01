0235b15dab0e59a7624578098952ad2bf5f5fcff
doremansunio <136595579+doremansunio@users.noreply.github.com>
2024-03-01 15:20:36 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,14 +1,6 @@
-variable "central_pool_name" {
-  type = string    
-}
-
 variable "project_name" {
   type = string  
 } 
-
-variable "cluster_name" {
-  type = string
-}
 
 variable "workspace_admins" {
   type    = list  
