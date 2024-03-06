d34c653cb75bc7264bd8e261861e6aa95a245fd5
doremansunio <136595579+doremansunio@users.noreply.github.com>
2024-03-06 07:45:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,18 +11,32 @@ provider "rafay" {
   # provider_config_file = "./rafay_config.json"
 }
 
-# Create new Rafay workspace group
-resource "rafay_group" "group-Workspace" {  
-  name        = "WrkspAdmin-grp-${var.project_name}"
-  description = "Workspace Admin Group for ${var.project_name}"  
-}
+# # Create new Rafay workspace group
+# resource "rafay_group" "group-Workspace" {  
+#   name        = "WrkspAdmin-grp-${var.project_name}"
+#   description = "Workspace Admin Group for ${var.project_name}"  
+# }
 
-# Creare new group assocication
-resource "rafay_groupassociation" "group-association" {
-  depends_on = [rafay_group.group-Workspace]
-  group      = "WrkspAdmin-grp-${var.project_name}"
-  project    = var.project_name
-  roles = ["WORKSPACE_ADMIN","ENVIRONMENT_TEMPLATE_USER"]
-  # add_users = ["${var.workspace_admins}"]
-  add_users = var.workspace_admins
+# # Creare new group assocication
+# resource "rafay_groupassociation" "group-association" {
+#   depends_on = [rafay_group.group-Workspace]
+#   group      = "WrkspAdmin-grp-${var.project_name}"
+#   project    = var.project_name
+#   roles = ["WORKSPACE_ADMIN","ENVIRONMENT_TEMPLATE_USER"]
+#   # add_users = ["${var.workspace_admins}"]
+#   add_users = var.workspace_admins
+# }
+
+resource "rafay_cluster_sharing" "demo-terraform-specific" {  
+  clustername = "eks-cluster"
+  project     = var.project_name
+
+  for_each = toset(var.workspace_admins)
+    sharing {
+        all = false
+    
+        projects {
+        name = each.value
+        }
+    }
 }
